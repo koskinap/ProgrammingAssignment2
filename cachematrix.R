2383f5a557b37f5f433ed5cbe0cafa58b0ad7c7b
koskinap <zenapolewn@gmail.com>
2015-05-24 14:56:09 +0300

    pa2 if-else

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,8 @@
-## The makeCacheMatrix function creates a "special vector",
-## which is a list with four functions performing operations regarding a matrix
+## The makeCacheMatrix function creates a "special matrix" object,
+## which is a list of four functions performing operations on a matrix,
+## acting as a cache memory
+## This function should be called every time we look for the inverse of a
+## new matrix, to make sure that the matrix in cache has not changed
 
 ## 1. set, sets the matrix in cache equal to that given
 ## 2. get, returns the matrix currenty in cache
@@ -29,21 +32,22 @@ makeCacheMatrix <- function(x = matrix()) {
 ## The "cacheSolve" function returns the inverse of a matrix.
 ## If the inverse of the matrix given,has already been calculated
 ## and stored in cache,it is recalled to avoid costly calculations,
-## otherwise it is calculated calling solve()
+## otherwise it is calculated calling solve(), and stored in cache via setinv()
 
 cacheSolve <- function(x, ...) {
      
         inv <- x$getinv()
-        
+
         if (!is.null(inv)){
                 message("Returning cached inverse")
-                inv
         }
-        
-        message("Calculating the inverse")
-        data <- x$get()
-        inv <- solve(data, ...)
-        x$setinv(inv)
+        else{
+                message("Calculating the inverse")
+                data <- x$get()
+                inv <- solve(data, ...)
+                x$setinv(inv)
+        }
+        ## Return the Inverse
         inv
                 
                 
